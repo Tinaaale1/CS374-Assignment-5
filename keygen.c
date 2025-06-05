a6f0e367bab5241374f58fcb5b07bb451d560f80
Tina A Le <let5@oregonstate.edu>
2025-06-05 08:00:06 +0000

    Add compileall

--- keygen.c
+++ keygen.c
@@ -2,48 +2,32 @@
 #include <stdlib.h>
 #include <time.h>
 
-// Function to check command-line argument count
-void check_args(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s keylength\n", argv[0]);
-        exit(EXIT_FAILURE);
+void usage(int argc, char *argv[]){
+    if (argc != 2){ 
+        fprintf(stderr, "Usage: %s length\n", argv[0]); 
+        exit(1);
     }
 }
 
-// argc is the number of command-line arguments 
-// argv is an array of strings of the command-line arguments
-int main(int argc, char* argv[]) {
-    // First argument is the program name 
-    // Second argument is the key length 
-    check_args(argc, argv);
+int main(int argc, char *argv[]){
+    usage(argc, argv);
 
-    // Convert the second command-line argument and store it in length 
     int length = atoi(argv[1]);
-    // If length is 0 or negative then error 
     if (length <= 0) {
-        fprintf(stderr, "Error: key length must be a positive integer\n");
-        return 1;
+        fprintf(stderr, "Error: key length must be positive integer\n");
+        exit(1);
     }
 
-    // Allocate space for the key (+1 for null terminator)
-    char key[length + 1];
+    char key[length + 1];  // Now length is known
 
-    // Random number generator with teh current time 
     srand(time(NULL));
 
-    // Fill the key with random characters from A-Z and space
-    for (int i = 0; i < length; i++) {
-        int r = rand() % 27;
-        if (r == 26) {
-            key[i] = ' ';
-        } else {
-            key[i] = 'A' + r;
-        }
+    for (int i = 0; i < length; i++){
+        key[i] = " ABCDEFGHIJKLMNOPQRSTUVWXYZ"[rand() % 27];
     }
+    key[length] = '\0';
 
-    key[length] = '\0'; // Null-terminate the key string
-
-    printf("%s\n", key);
+    printf("%s\n", key);  // outputs the key plus a newline
 
     return 0;
 }
